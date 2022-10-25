0785266d88840992a1ebe8042026bac80291aa73
ksungsu <zxzxzlxlzlxl@naver.com>
2022-10-25 20:49:27 +0900

    20221024 study spring security

--- shop/src/main/java/com/example/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/example/shop/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,10 @@ import javax.servlet.http.HttpServlet;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
