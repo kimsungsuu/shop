a0f192b6b732ffdb6e10d2e03f05c2db7fdeab37
ksungsu <zxzxzlxlzlxl@naver.com>
2022-10-30 19:45:44 +0900

    20221030 Study 연관 관계 매핑

---
+++ shop/src/main/java/com/example/shop/entity/Cart.java
@@ -0,0 +1,24 @@
+package com.example.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@ToString
+@Table(name="cart")
+public class Cart {
+
+    @Id
+    @Column(name="cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne() // cart -> member_id (단방향)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+}
